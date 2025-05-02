499a30463aa01a7aabcb8dfa1f919e53b1d7b57e
zami26 <arafatulislamzami@gmail.com>
2025-05-03 00:52:49 +0600

    all cart done

--- js/donate.js
+++ js/donate.js
@@ -3,14 +3,25 @@ function donateNow(btnIdToDonate,amountFromInput,amountAlreadyHave){
         donationAmountText=document.getElementById(amountFromInput).value;
         alreadyHavingText=document.getElementById(amountAlreadyHave).innerText;
         ownedAmountText= document.getElementById("owned-amount").innerText;
+        ownedAmountForPhoneText= document.getElementById("update-for-phone").innerText;
+        // console.log(donationAmountText);
+                       
         donationAmount=Number(donationAmountText);
         alreadyHaving=Number(alreadyHavingText);
         ownedAmount=Number(ownedAmountText);
+        ownedAmountForPhone=Number(ownedAmountForPhoneText);
+        // console.log(donationAmount,alreadyHaving,ownedAmount,ownedAmountForPhone);
         newAmount=alreadyHaving+donationAmount;
         restAmount=ownedAmount-donationAmount;
-        document.getElementById("noa-having-amount").innerText=newAmount;
+        restAmountForPhone=ownedAmountForPhone-donationAmount;
+        document.getElementById(amountAlreadyHave).innerText=newAmount;
         document.getElementById("owned-amount").innerText=restAmount;
+        document.getElementById("update-for-phone").innerText=restAmountForPhone;
+
+        document.getElementById(amountFromInput).value='';
       })
 }
 
 donateNow("donate-noa","donating-amount","noa-having-amount");
+donateNow("donate-feni", "donating-amount-feni", "feni-having-amount");
+donateNow("donate-student", "donating-amount-student", "student-having-amount");
