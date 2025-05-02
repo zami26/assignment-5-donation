687080e00dd3a4889f7daa0dcbd830ae9d4b3d7f
zami26 <arafatulislamzami@gmail.com>
2025-05-03 02:45:10 +0600

    history part done

--- js/donate.js
+++ js/donate.js
@@ -1,4 +1,4 @@
-function donateNow(btnIdToDonate,amountFromInput,amountAlreadyHave){
+function donateNow(btnIdToDonate,amountFromInput,amountAlreadyHave,reason){
     document.getElementById(btnIdToDonate).addEventListener('click',function(event){
         donationAmountText=document.getElementById(amountFromInput).value;
         alreadyHavingText=document.getElementById(amountAlreadyHave).innerText;
@@ -18,10 +18,35 @@ function donateNow(btnIdToDonate,amountFromInput,amountAlreadyHave){
         document.getElementById("owned-amount").innerText=restAmount;
         document.getElementById("update-for-phone").innerText=restAmountForPhone;
 
+        occar=document.getElementById(reason).innerText;
+// adding history
+        const newDiv=document.createElement("div");
+        newDiv.classList="border  border-gray-300 px-6 py-5 rounded mb-5 font-bold";
+
+        const p = document.createElement("p");
+        p.innerText = `${donationAmount} Taka is Donated for ${occar}`;
+        const p2=document.createElement("p");
+        p2.innerText = `Date: ${new Date().toString()}`;
+        newDiv.appendChild(p);
+        newDiv.appendChild(p2);
+    
+        document.getElementById("history-details").appendChild(newDiv);
+
         document.getElementById(amountFromInput).value='';
       })
 }
 
-donateNow("donate-noa","donating-amount","noa-having-amount");
-donateNow("donate-feni", "donating-amount-feni", "feni-having-amount");
-donateNow("donate-student", "donating-amount-student", "student-having-amount");
+donateNow("donate-noa","donating-amount","noa-having-amount", "first");
+donateNow("donate-feni", "donating-amount-feni", "feni-having-amount","second");
+donateNow("donate-student", "donating-amount-student", "student-having-amount","third");
+
+
+document.getElementById("history").addEventListener('click',function(){
+    document.getElementById("cart-sec").classList.add("hidden");
+    document.getElementById("history-sec").classList.remove("hidden");
+})
+document.getElementById("donation").addEventListener('click',function(){
+    document.getElementById("cart-sec").classList.remove("hidden");
+    document.getElementById("history-sec").classList.add("hidden");
+
+})
